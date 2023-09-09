940d87073c7f6864e064624446bfeb55a9331594
Quentin Orhant <quentin.orhant@gmail.com>
2023-09-10 00:22:35 +0200

    add draft function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_07_111401) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_09_213812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_111401) do
     t.text "source"
     t.text "script"
     t.date "date"
+    t.boolean "draft", default: false
   end
 
   create_table "taggings", force: :cascade do |t|
