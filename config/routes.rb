97148fc9c2be14b5f6959a7989f47d7547f71035
Quentin Orhant <quentin.orhant@gmail.com>
2023-09-13 14:55:34 +0200

    change posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,18 @@ Rails.application.routes.draw do
   resources :photos
   resources :categories
   devise_for :users
-  resources :posts do
-    member do
-      patch :delete_photo
-    end
-  end
-  root to: "posts#index"
+
   get 'tags/:tag', to: 'posts#index', as: :tagged
   resources :contacts, only: [:new, :create]
   get 'contact', to: 'contacts#new', as: 'contact'
   get 'message', to: 'contacts#new2'
   get 'contacts/sent'
+
+  resources :posts, path: '/' do
+    member do
+      patch :delete_photo
+    end
+  end
+
+  root to: "posts#index"
 end
