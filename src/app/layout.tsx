71c28c3af3dfae3d24ae6dd80e5d22c4b5f9a91b
Quentin Orhant <quentin.orhant@mailo.fr>
2026-01-30 16:20:26 +0100

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,13 +24,13 @@ export const metadata: Metadata = {
     type: "website",
     locale: "fr_FR",
     title: "Quentin Orhant - Maker et développeur freelance",
-    description: "Portfolio de projets entre écologie et numérique",
+    description: "Portfolio de Quentin Orhant, maker et développeur freelance spécialisé dans les projets entre écologie et numérique. Prototypage, programmation, médiation et créations artistiques.",
     siteName: "Quentin Orhant",
   },
   twitter: {
     card: "summary_large_image",
     title: "Quentin Orhant - Maker et développeur freelance",
-    description: "Portfolio de projets entre écologie et numérique",
+    description: "Portfolio de Quentin Orhant, maker et développeur freelance spécialisé dans les projets entre écologie et numérique. Prototypage, programmation, médiation et créations artistiques.",
   },
   robots: {
     index: true,
